00119b03e21fa25d97ea88a33fea0a18cabc05aa
Manuel-Gonzalez1 <60111293+Manuel-Gonzalez1@users.noreply.github.com>
2020-01-22 12:59:03 -0700

    Hola mundo

---
+++ Clase_1/Practica1/Program.cs
@@ -0,0 +1,13 @@
+using system;
+//importamos la libreria principal
+
+namespace practica1
+{
+    class program //Esta es una clase base
+    {
+        static void Main(string args)
+        {
+            Console.WriteLine("Hola Mundo!");
+        } 
+    }
+}
