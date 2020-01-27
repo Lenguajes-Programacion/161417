4033b6cd8a2153861aa0e75949a2cf32e89fde98
Manuel-Gonzalez1 <60111293+Manuel-Gonzalez1@users.noreply.github.com>
2020-01-27 12:31:46 -0700

    Update Program.cs

--- Clase_1/Practica1/Program.cs
+++ Clase_1/Practica1/Program.cs
@@ -5,9 +5,13 @@ namespace practica1
 {
     class program //Esta es una clase base
     {
+        //Variable de la Clase.
+        static string nombre = "Manuel";
+
         static void Main(string[] args) //args = {"Manuel","20","Mexicano"}Arreglo de informacion personal
         {
-            Console.WriteLine($"Hello {args[0]}!");
+            //Respondemos en el primer argumento
+            Console.WriteLine($"Hello {args[0]} {args[1]}");
         } 
     }
 }
