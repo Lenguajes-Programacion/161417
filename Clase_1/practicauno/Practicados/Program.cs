4afc0d34dcfdbebbd0adc2b95dfd807afd5e3a02
Manuel-Gonzalez1 <60111293+Manuel-Gonzalez1@users.noreply.github.com>
2020-02-07 12:51:58 -0700

    Create Program.cs

---
+++ Clase_1/practicauno/Practicados/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practicados
+{
+    class Program
+    {
+        //Proyecto Calculadora
+        //Se espera recivir 2 argumentos valor1 (Int), valor2 (String) operador
+        //valor3 (int)
+        //Responder resultado en (int)
+        static void Main(string[] args)
+        {
+            switch (args[1])
+            {
+                case "+":
+                    Console.WriteLine("Es una suma");//Detectar tipo de operador                   
+                    break;
+                default:
+                    Console.WriteLine("Los argumentos no cuentan con un operador valido:" + args[1]);
+                    break;
+            }
+        }
+    }
+}
