ba7c552bf68775dc0eee2ff06a6a01321c0f29af
Manuel-Gonzalez1 <60111293+Manuel-Gonzalez1@users.noreply.github.com>
2020-02-24 12:46:18 -0700

    Update Class1.cs

--- Clase_1/practicauno/Practicados/Class1.cs
+++ Clase_1/practicauno/Practicados/Class1.cs
@@ -6,25 +6,41 @@ using System.Text;
 
 namespace Practicados
 {
-    class Class1
+    class Memoria
     {
         public void LeerMemoria()
         {
             string archivoDB = "../../../db.json";//Ubicación del archivo
             StreamReader reader = new StreamReader(archivoDB);//Lee en tiempo real el archivo
             var dbJSON = reader.ReadToEnd();
-            //Prueba de lectura
             var dbObject = JObject.Parse(dbJSON);//Hacemos que lo interprete
-            var result = dbObject.ToString();
+            //Prueba de lectura
+            //var result = dbObject.ToString();
             //var result = dbObject["arreglo"].ToString();//Carga lo que esta en el arreglo
             //var result = dbObject["arreglo"][0].ToString();//Carga lo que esta en el arreglo en el nivel 0
 
             //Lectura de nuestro json iterable
-            foreach (var item in dbObject)//item=grupo de memoria
+            foreach ((var key,var item) in dbObject)//item=grupo de memoria
             {
                 Console.WriteLine("Dato en memoria:");
-                Console.WriteLine(result.ToString());
+                MemoriaData memoriaData = new MemoriaData(DateTime.Now,item["operacion"].ToString(),(int) item["resultado"]);
+                Console.WriteLine(key.ToString());
+                Console.WriteLine(memoriaData.resultado.ToString());
             }
         }
     }
+    class MemoriaData
+    {
+        public DateTime fecha;
+        public String operacion;
+        public int resultado;
+
+        public MemoriaData(DateTime date,String operation,int result){
+            fecha = date;
+            operacion = operation;
+            resultado = result;
+        }
+    }
+
 }
+//Agregar formato a la fecha, tipo de operacion y resultado
